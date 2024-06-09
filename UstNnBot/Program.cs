bfe16085a9e4ccb2b43f5c0462740adebcabdcf6
anschek <anschek@yandex.ru>
2024-06-10 00:16:49 +0300

    feat: add UstBot class (no actions now)

--- UstNnBot/Program.cs
+++ UstNnBot/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            UstBot ustBot = new UstBot("7439368344:AAGVuhh5iEzTQ2YXgQy6i3_VzwtfT30YVYc");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
---
+++ UstNnBot/UstBot.cs
@@ -0,0 +1,25 @@
+﻿using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace UstNnBot
+{
+    internal class UstBot
+    {
+        private static TelegramBotClient _botClient;
+        internal UstBot(string token)
+        {
+            _botClient = new TelegramBotClient(token);
+            _botClient.StartReceiving(Update, Error);
+        }
+
+        private static Task Update(ITelegramBotClient client, Update update, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
