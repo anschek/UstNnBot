00a208e7347d31cc6e75d8a90a8417b34ad2b0c5
anschek <anschek@yandex.ru>
2024-06-11 00:00:14 +0300

    feat: add .dll file with db connect and try get data

--- UstNnBot/Program.cs
+++ UstNnBot/Program.cs
@@ -1,12 +1,18 @@
-﻿namespace UstNnBot
+﻿using DatabaseLibrary.Entities.ComponentCalculationProperties;
+using DatabaseLibrary.Queries;
+
+namespace UstNnBot
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            UstBot ustBot = new UstBot("7439368344:AAGVuhh5iEzTQ2YXgQy6i3_VzwtfT30YVYc");
-            Console.WriteLine("Press any key to exit");
-            Console.ReadKey();
+            //UstBot ustBot = new UstBot("7439368344:AAGVuhh5iEzTQ2YXgQy6i3_VzwtfT30YVYc");
+            //Console.WriteLine("Press any key to exit");
+            //Console.ReadKey();
+
+
+            List<ComponentCalculation>? components = GET.View.ComponentCalculationsBy(5218);
         }
     }
 }
