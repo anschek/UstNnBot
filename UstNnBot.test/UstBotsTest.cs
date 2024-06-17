b10827f66e696b849a34674b62a483e118c81b8e
anschek <anschek@yandex.ru>
2024-06-17 13:27:58 +0300

    test: check method 'FilterOneProcurement', 1/4 test failed

--- UstNnBot.test/UstBotsTest.cs
+++ UstNnBot.test/UstBotsTest.cs
@@ -1,5 +1,6 @@
 using DatabaseLibrary.Entities.Actions;
 using DatabaseLibrary.Entities.ComponentCalculationProperties;
+using DatabaseLibrary.Entities.EmployeeMuchToMany;
 using DatabaseLibrary.Queries;
 
 namespace UstNnBot.test
@@ -13,13 +14,13 @@ namespace UstNnBot.test
             int procurementId = 5678;
             var returnsDict = UstBot.GetComponentsWithHeaders(procurementId);
             var expectedKeys = GET.View.ComponentCalculationsBy(procurementId)
-                .Where(component => component.IsHeader==true && component.IsDeleted==false);
+                .Where(component => component.IsHeader == true && component.IsDeleted == false);
             var expectedValues = GET.View.ComponentCalculationsBy(procurementId)
-                .Where(component => component.IsHeader==false && component.IsDeleted==false);
-            foreach((var key, var list) in returnsDict)
+                .Where(component => component.IsHeader == false && component.IsDeleted == false);
+            foreach ((var key, var list) in returnsDict)
             {
                 Assert.AreEqual(1, expectedKeys.Count(component => component.Id == key.Id));
-                foreach(var value in list) Assert.AreEqual(1, expectedValues.Count(component => component.Id == value.Id));
+                foreach (var value in list) Assert.AreEqual(1, expectedValues.Count(component => component.Id == value.Id));
             }
             Assert.AreEqual(expectedKeys.Count(), returnsDict.Count);
 
@@ -37,7 +38,7 @@ namespace UstNnBot.test
             int procurementId = 5678;
             var returnsList = UstBot.GetTechnicalComments(procurementId);
             var expectedList = GET.View.CommentsBy(procurementId, isTechical: true);
-            foreach(var value in returnsList)
+            foreach (var value in returnsList)
                 Assert.AreEqual(1, expectedList.Count(comment => comment.Id == value.Id));
             Assert.AreEqual(expectedList.Count(), returnsList.Count);
         }
@@ -97,23 +98,44 @@ namespace UstNnBot.test
         [TestMethod]
         public void FilterOneProcurement_SomeUsersAreAllowed_ReturnsUserIdsList()
         {
-
+            var procurementsEmployees = new List<ProcurementsEmployee>
+            {
+                new ProcurementsEmployee { Employee = new Employee { UserName = "catInTeapot" }, EmployeeId = 1 },
+                new ProcurementsEmployee { Employee = new Employee { UserName = "clownFish"   }, EmployeeId = 2 }
+            };
+            var allowedUsers = new List<string> { "catInTeapot" };
+            var result = UstBot.FilterOneProcurement(procurementsEmployees, allowedUsers);
+            CollectionAssert.AreEqual(new List<int> { 1 }, result);
         }
         [TestMethod]
         public void FilterOneProcurement_NoUserIsAllowed_ReturnsEmptyList()
         {
-
+            var procurementsEmployees = new List<ProcurementsEmployee>
+            {
+                new ProcurementsEmployee { Employee = new Employee { UserName = "catInTeapot" }, EmployeeId = 1 },
+                new ProcurementsEmployee { Employee = new Employee { UserName = "clownFish"   }, EmployeeId = 2 }
+            };
+            var allowedUsers = new List<string> { "denZel" };
+            var result = UstBot.FilterOneProcurement(procurementsEmployees, allowedUsers);
+            Assert.AreEqual(0, result.Count);
         }
-        [TestMethod]
+        [TestMethod]//throws  System.ArgumentNullException: Value cannot be null.
         public void FilterOneProcurement_ProcurementsEmployeesListIsNull_ReturnsNull()
         {
-
+            List<ProcurementsEmployee>? procurementsEmployees = null;
+            var allowedUsers = new List<string> { "catInTeapot" };
+            var result = UstBot.FilterOneProcurement(procurementsEmployees, allowedUsers);
+            Assert.IsNull(result);
         }
         [TestMethod]
-        public void FilterOneProcurement_ProcurementsEmployeesListIsEmpty_ReturnsNull()
+        public void FilterOneProcurement_ProcurementsEmployeesListIsEmpty_ReturnsEmptyList()
         {
-
+            var procurementsEmployees = new List<ProcurementsEmployee>();
+            var allowedUsers = new List<string> { "catInTeapot" };
+            var result = UstBot.FilterOneProcurement(procurementsEmployees, allowedUsers);
+            Assert.AreEqual(0, result.Count);
         }
+
         [TestMethod]
         public void FilterProcurements_CorrectIndividualData_ReturnsEmployeePlan()
         {
